eae62361c797a9f517a1a1f4c1a4ffdaed2b2e3c
ibiscum <ulf.luecking@gmail.com>
2025-12-26 15:23:29 +0100

    ch02

--- chapter-02/code-samples/ch02_types_and_operations.js
+++ chapter-02/code-samples/ch02_types_and_operations.js
@@ -4,7 +4,7 @@ let str1 = "Hello ";
 let str2 = "addition";
 let result1 = nr1 + nr2;
 let result2 = str1 + str2;
-console.log(result1, result2);
+console.log("ex_23:", result1, result2);
 
 nr1 = 20;
 nr2 = 4;
@@ -12,7 +12,7 @@ str1 = "Hi ";
 let nr3 = 3;
 result1 = nr1 - nr2;
 result2 = str1 - nr3;
-console.log(result1, result2);
+console.log("ex_24:", result1, result2);
 
 nr1 = 15;
 nr2 = 10;
@@ -20,45 +20,45 @@ str1 = "Hi";
 nr3 = 3;
 result1 = nr1 * nr2;
 result2 = str1 * nr3;
-console.log(result1, result2);
+console.log("ex_25:", result1, result2);
 
 nr1 = 30;
 nr2 = 5;
 result1 = nr1 / nr2;
-console.log(result1);
+console.log("ex_26:", result1);
 
-// let nr1 = 2;
-// let nr2 = 3;
-// let result1 = nr1 ** nr2;
-// console.log(result1);
+nr1 = 2;
+nr2 = 3;
+result1 = nr1 ** nr2;
+console.log("ex_27:", result1);
 
-// let nr1 = 10;
-// let nr2 = 3;
-// let result1 = nr1 % nr2;
-// console.log(`${nr1} % ${nr2} = ${result1}`);
+nr1 = 10;
+nr2 = 3;
+result1 = nr1 % nr2;
+console.log("ex_28:", `${nr1} % ${nr2} = ${result1}`);
 
-// let nr3 = 8;
-// let nr4 = 2;
-// let result2 = nr3 % nr4;
-// console.log(`${nr3} % ${nr4} = ${result2}`);
+nr3 = 8;
+let nr4 = 2;
+result2 = nr3 % nr4;
+console.log("ex_29:", `${nr3} % ${nr4} = ${result2}`);
 
-// let nr5 = 15;
-// let nr6 = 4;
-// let result3 = nr5 % nr6;
-// console.log(`${nr5} % ${nr6} = ${result3}`);
+let nr5 = 15;
+let nr6 = 4;
+let result3 = nr5 % nr6;
+console.log("ex_30:", `${nr5} % ${nr6} = ${result3}`);
 
-// let nr = 4;
-// nr++;
-// console.log(nr);
+let nr = 4;
+nr++;
+console.log("ex_31:", nr);
 
-// let nr = 2;
-// console.log(nr++);
-// console.log(nr);
+nr = 2;
+console.log("ex_32:", nr++);
+console.log("ex_33:", nr);
 
-// let nr = 2;
-// console.log(++nr);
+nr = 2;
+console.log("ex_34:", ++nr);
 
-//let nr1 = 4;
-//let nr2 = 5;
-//let nr3 = 2;
-//console.log(nr1++ + ++nr2 * nr3++);
+nr1 = 4;
+nr2 = 5;
+nr3 = 2;
+console.log("ex_35:", nr1++ + ++nr2 * nr3++);
--- chapter-02/code-samples/ch02_undefined_null.js
+++ chapter-02/code-samples/ch02_undefined_null.js
@@ -1,12 +1,12 @@
 let unassigned;
-console.log(unassigned);
+console.log("ex_36:", unassigned);
 
 let terribleThingToDo = undefined;
 let lastname;
-console.log("Same undefined:", lastname === terribleThingToDo);
+console.log("ex_37:", lastname === terribleThingToDo);
 
 let betterOption = null;
-console.log("Same null:", lastname === betterOption);
+console.log("ex_38:", lastname === betterOption);
 
 let empty = null;
-console.log("empty is null:", empty === null);
+console.log("ex_39:", empty === null);
