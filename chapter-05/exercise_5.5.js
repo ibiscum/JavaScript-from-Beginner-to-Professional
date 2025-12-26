e2400a07303a43948cb7bc76859113206e6229cb
ibiscum <ulf.luecking@gmail.com>
2025-12-27 00:29:00 +0100

    ch05

--- chapter-05/exercise_5.5.js
+++ chapter-05/exercise_5.5.js
@@ -3,15 +3,15 @@ const cells = 64;
 let counter = 0;
 let row;
 for (let x = 0; x < cells + 1; x++) {
-    if (counter % 8 == 0) {
-        if (row != undefined) {        
-            grid.push(row);
-        }
-        row = [];
+  if (counter % 8 == 0) {
+    if (row != undefined) {
+      grid.push(row);
     }
-    counter++;
-    let temp = counter;
-    row.push(temp);
- 
+    row = [];
+  }
+  counter++;
+  let temp = counter;
+  row.push(temp);
+
 }
 console.table(grid);
--- chapter-05/exercise_5.6.js
+++ chapter-05/exercise_5.6.js
@@ -1,12 +1,12 @@
 const myArray = [];
 for (let x = 0; x < 10; x++) {
-    myArray.push(x + 1);
+  myArray.push(x + 1);
 }
 console.log(myArray);
 
 for (let i = 0; i < myArray.length; i++) {
-    console.log(myArray[i]);
+  console.log(myArray[i]);
 }
 for (let val of myArray) {
-    console.log(val);
+  console.log(val);
 }
