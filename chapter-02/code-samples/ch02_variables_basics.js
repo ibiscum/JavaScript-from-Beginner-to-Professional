5313e05c4e5defc5c3d018a9c3194b5ac2257f5c
ibiscum <ulf.luecking@gmail.com>
2025-12-26 21:40:00 +0100

    ch02

--- chapter-02/code-samples/ch02_variables_basics.js
+++ chapter-02/code-samples/ch02_variables_basics.js
@@ -1,14 +1,13 @@
 let firstname = "Maria";
 firstname = "Jacky";
-
-console.log(firstname);
+console.log("ex_40:", firstname);
 
 let nr1 = 12;
 var nr2 = 8;
 const pi = 3.14159;
-console.log(nr1, nr2, pi);
+console.log("ex_41:", nr1, nr2, pi);
 
 // throws a TypeError
 const someConstant = 3;
 // someConstant = 4;
-console.log(someConstant);
+console.log("ex_42:", someConstant);
--- chapter-02/code-samples/ch02_variables_number.js
+++ chapter-02/code-samples/ch02_variables_number.js
@@ -1,16 +1,16 @@
 let intNr = 1;
 let decNr = 1.5;
-console.log(intNr, decNr);
+console.log("ex_43:", intNr, decNr);
 
 let hexNr = 0xff;
 let binNr = 0b1010;
 let octNr = 0o744;
-console.log(hexNr, binNr, octNr);
+console.log("ex_44:", hexNr, binNr, octNr);
 
 let expNr = 1.4e15;
-console.log(expNr);
+console.log("ex_45:", expNr);
 
 let bigNr = 90071992547409920n;
 // typeError
-let result = bigNr + intNr;
-console.log(result);
+let result = bigNr + BigInt(intNr);
+console.log("ex_46:", result);
