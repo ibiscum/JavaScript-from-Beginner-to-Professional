bdb6511058253ecbd2c342e4f60a9e0e71fd05e2
ibiscum <ulf.luecking@gmail.com>
2026-03-22 21:00:12 +0100

    update

--- chapter-03/exercise_3.2.js
+++ chapter-03/exercise_3.2.js
@@ -1,14 +1,24 @@
 const myList = [];
+console.log(myList);
+
 myList.push("Milk", "Bread", "Apples");
+console.log(myList);
+
 myList.splice(1, 1, "Bananas", "Eggs");
+console.log(myList);
+
 const removeLast = myList.pop();
 console.log(removeLast);
 
 myList.sort();
+console.log(myList);
 console.log(myList.indexOf("Milk"));
 
 myList.splice(1, 0, "Carrots", "Lettuce");
+console.log(myList);
+
 const myList2 = ["Juice", "Pop"];
 const finalList = myList.concat(myList2, myList2);
-console.log(finalList.lastIndexOf("Pop"));
 console.log(finalList);
+
+console.log(finalList.lastIndexOf("Pop"));
--- chapter-08/code-samples/ch08_arrays.js
+++ chapter-08/code-samples/ch08_arrays.js
@@ -1,6 +1,6 @@
 let arr = ["grapefruit", 4, "hello", 5.6, true];
 function printStuff(element, index) {
-    console.log("Printing stuff:", element, "on array position:", index);
+  console.log("01 Printing stuff:", element, "on array position:", index);
 }
 
 printStuff(arr[0], 0);
@@ -10,19 +10,23 @@ function checkString(element) {
   return typeof element === "string";
 }
 let filterArr = arr.filter(checkString);
-console.log(filterArr);
+console.log("02 filter:", filterArr);
 
-console.log(arr.every(checkString));
+console.log("03 every:", arr.every(checkString));
 
 arr = ["grapefruit", 4, "hello", 5.6, true];
 arr.copyWithin(0, 3, 4);
+console.log("04 copyWithin:", arr);
+
 
 arr = ["grapefruit", 4, "hello", 5.6, true];
 arr.copyWithin(0, 3, 5);
+console.log("05 copyWithin:", arr);
+
 
 arr = ["grapefruit", 4, "hello", 5.6, true, false];
 arr.copyWithin(0, 3);
-console.log(arr);
+console.log("06 copyWithin:", arr);
 
 arr = [1, 2, 3, 4];
 let mapped_arr = arr.map(x => x + 1);
@@ -35,9 +39,9 @@ bb = ["so", "bye", "bye", "love"];
 console.log(bb.lastIndexOf("hi"));
 
 let letters = ["a", "b", "c"];
-x = letters.join();
+let x = letters.join();
 console.log(x);
 
 letters = ["a", "b", "c"];
-let x = letters.join('-');
+x = letters.join('-');
 console.log(x);
